6ceb6d46bda2de8f09f41e2829cc5e409a8d1359
aolyu <l>
2020-07-28 17:43:59 +0800

    fix: 修复页面不能正常渲染问题；优化ts代码规范

--- src/components/EventEmitterButton.tsx
+++ src/components/EventEmitterButton.tsx
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 import { layoutEmitter } from '@/utils/EventEmitter';
+// 定义样式文件
+import styles from './EventEmitterButton.less';
 
-export default ({ initNumber }) => {
+// 自定义类型值，表示可允许的值，可多个
+interface MyType {
+    initNumber: number;
+}
+
+// 和上面interface对应，表示有一个对象，其中有一个值为initNumber且它的类型为number
+export default ({ initNumber }: MyType) => {
 
     const [state, setstate] = useState(initNumber);
 
+    // 去掉内联样式，写到less中
     return <button
-        style={{ fontSize: '30px' }}
+        className={styles.myBtnFontStyle}
         onClick={() => {
-            setstate(state + 1)
-            layoutEmitter.emit({ state });
+            setstate(state + 1);
+            layoutEmitter.emit({ state });  // 执行layoutEmitter所绑定的所有事件。
         }}
     >EventEmitter {state} </button>
 };
