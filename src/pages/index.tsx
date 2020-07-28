65b7d4e5ffc7e554dc656282212e6179f0f53e99
iFoli <42626941+iFoli@users.noreply.github.com>
2020-07-28 17:58:13 +0800

    fix: 更正按钮点击时，UI DOM展示state不一致问题，正确渲染list的长度，补充TS参数类型的定义

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,35 @@ import React, { useEffect, useState } from 'react';
 import { layoutEmitter } from '@/utils/EventEmitter';
 import EventEmitterButton from '@/components/EventEmitterButton';
 
+interface ItemProps {
+    state: number;
+}
+
+interface State {
+    state: number
+}
 export default () => {
-    const [list, setList] = useState([]);
+    const [list, setList] = useState([] as Array<State>);
 
     useEffect(() => {
         layoutEmitter.useSubscription((data) => {
             list.push(data);
             console.log(data);
             console.log(list);
-            setList(list);
+            // 4. list 为数组，引用数据类型，我们不能直接操作state要结构出来
+            const listData = [...list]
+            setList(listData);
         });
     }, [])
+
     return (
         <div>
             <EventEmitterButton initNumber={11} />
             <p>list length:{list.length}</p>
             {
-                list.map(item => <p>{item.state}</p>)
+                // 5. item中state的参数类型
+                // 6. 通过map循环需要给标签加唯一的key
+                list.map((item: ItemProps) => <p key={item.state}>{item.state}</p>)
             }
         </div>
     )
